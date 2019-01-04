d774c5745c8c1f3c562d5363da156f92c7e43ce4
wow <wow@gskynet.vn>
2019-01-04 10:59:24 +0700

    compile without error

--- real_code.hs
+++ real_code.hs
@@ -7,6 +7,7 @@ import           Data.Either
 import qualified Data.List                  as List
 import           Data.Map.Strict
 import           Data.Word
+import           Debug.Trace                (trace, traceShow)
 import           Hapstone.Capstone
 import           Hapstone.Internal.Capstone as Capstone
 import           Numeric                    (showHex)
@@ -24,10 +25,10 @@ data Registers = Registers {
 } deriving (Show)
 
 data ProccessedInsn = NormalInsn CsInsn | JunkInsn String deriving (Show)
-type ProccessedInsnList = [ProccessedInsn]
+type ProccessedInsnList = [(Word64, ProccessedInsn)]
 
 data State = State {
-  blocks :: Map Int (Maybe ProccessedInsnList),
+  blocks :: Map Word64 (Maybe ProccessedInsnList),
   regs   :: Registers
 } deriving (Show)
 
@@ -51,7 +52,7 @@ new_state = State {blocks=init_blocks,regs=Registers {eax=0, ebx=0, ecx=0, edx=0
   where
     init_blocks = fromList [(0x1DBF71A, Nothing)]
 
-is_unproccessed_block :: (Int, Maybe ProccessedInsnList) -> Bool
+is_unproccessed_block :: (Word64, Maybe ProccessedInsnList) -> Bool
 is_unproccessed_block (_, Nothing) = True
 is_unproccessed_block (_, _)       = False
 
@@ -62,27 +63,46 @@ lift_next_block state contents = do
     Nothing    -> return state
     Just block -> lift_block state contents block
 
-lift_block :: State -> BS.ByteString -> (Int, Maybe ProccessedInsnList) -> IO State
+lift_block :: State -> BS.ByteString -> (Word64, Maybe ProccessedInsnList) -> IO State
 lift_block state _ (_, Just _)                  = return state
 lift_block state contents (start_addr, Nothing) = do
   insn_list <- disasm_block contents start_addr
   new_state <- case insn_list of
     Left err        -> lift_next_block state contents
-    Right insn_list -> return $ set_block_content state start_addr insn_list
+    Right insn_list -> return $ do_lift_block state start_addr insn_list []
   return new_state
 
-disasm_block :: BS.ByteString -> Int -> IO (Either CsErr [CsInsn])
-disasm_block contents start_addr = disasmSimpleIO $ disasm bin (fromIntegral(start_addr) :: Word64)
+disasm_block :: BS.ByteString -> Word64 -> IO (Either CsErr [CsInsn])
+disasm_block contents start_addr = disasmSimpleIO $ disasm bin start_addr
   where
-    bin = getBinPart contents start_addr 100
+    bin = getBinPart contents sa 100
+    sa = fromIntegral(start_addr)::Int
 
--- normal_insn i = NormalInsn i
+do_lift_block :: State -> Word64 -> [CsInsn] -> [Word64] -> State
+do_lift_block state _ [] _ = state
+do_lift_block state bl_addr (insn:xs) addr_stack = do
+  let new_as = bl_addr : addr_stack
+  let insn_size = fromIntegral(length $ bytes insn)::Word64
+  let next_addr = (address insn) + insn_size
+  let new_state = add_block_content state bl_addr $ NormalInsn insn
+  do_lift_block new_state bl_addr xs new_as
 
-set_block_content :: State -> Int -> [CsInsn] -> State
-set_block_content state addr insn_list = do
-  let ct = Just $ List.map NormalInsn insn_list
-  let bl = insert addr ct $ blocks state
-  state { blocks = bl}
+-- set_block_content :: State -> Int -> [CsInsn] -> State
+-- set_block_content state addr insn_list = do
+--   let ct = Just $ List.map NormalInsn insn_list
+--   let bl = insert addr ct $ blocks state
+--   state { blocks = bl}
+
+add_block_content :: State -> Word64 -> ProccessedInsn -> State
+add_block_content state addr insn = do
+  let i = (addr, insn)
+  let bls = blocks state
+  case bls ! addr of
+    Nothing -> state { blocks = insert addr (Just [i]) bls }
+    Just bl -> do
+      let ct = i : bl
+      let xxx = insert addr (Just ct) bls
+      state { blocks = xxx }
 
 main :: IO ()
 main = do
