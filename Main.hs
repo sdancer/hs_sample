6386bf24a9a928fbb14aa3ef0ce27756e1dd164a
wow <wow@gskynet.vn>
2019-01-09 10:31:10 +0700

    add max instruction count

--- Main.hs
+++ Main.hs
@@ -23,16 +23,16 @@ import           Util
 lift_next_block :: State -> IO State
 lift_next_block state = case blocks_queue state of
     x:xs    -> do
-      nstate <- trace (">>>> lift next block " ++ showHex x "") $ start_lift_block state x []
+      nstate <- trace (">>>> lift next block " ++ showHex x "") $ start_lift_block state x [] 100000
       lift_next_block $ nstate { blocks_queue=xs }
     otherwise -> return state
   where
     contents = mem_data state
 
-start_lift_block :: State -> BlockAddr -> [InsnAddr] -> IO State
-start_lift_block state bl_addr addr_stack = trace (">>> start lift " ++ showHex bl_addr "") $ if is_proccessed_block bl_addr state -- no infinity loop
+start_lift_block :: State -> BlockAddr -> [InsnAddr] -> Int -> IO State
+start_lift_block state bl_addr addr_stack cnt = trace (">>> start lift " ++ showHex bl_addr " " ++ show(keys $ blocks state)) $ if is_proccessed_block bl_addr state -- no infinity loop
   then trace ("--| already proccessed block 0x" ++ showHex bl_addr "") $ return state
-  else lift_block state bl_addr bl_addr addr_stack
+  else lift_block state bl_addr bl_addr addr_stack 0 cnt
 
 is_proccessed_block :: BlockAddr -> State -> Bool
 is_proccessed_block bl_addr state = List.elem bl_addr $ keys $ blocks state
@@ -40,9 +40,9 @@ is_proccessed_block bl_addr state = List.elem bl_addr $ keys $ blocks state
 is_proccessed_insn :: InsnAddr -> [InsnAddr] -> Bool
 is_proccessed_insn insn_addr addr_stack = List.elem insn_addr addr_stack
 
-lift_block :: State -> BlockAddr -> InsnAddr -> [InsnAddr] -> IO State
-lift_block state bl_addr start_addr addr_stack = if is_proccessed_insn start_addr addr_stack
-  then trace ("-- already proccessed insn 0x" ++ showHex start_addr "") $ return state
+lift_block :: State -> BlockAddr -> InsnAddr -> [InsnAddr] -> Int -> Int -> IO State
+lift_block state bl_addr start_addr addr_stack cnt cnt_max = if cnt == cnt_max || is_proccessed_insn start_addr addr_stack
+  then trace ("-- already proccessed insn 0x" ++ showHex start_addr " or max " ++ show cnt ++ " " ++ show cnt_max) $ return state
   else do
     insn_list <- disasm_block contents start_addr
     new_state <- case insn_list of
@@ -50,7 +50,7 @@ lift_block state bl_addr start_addr addr_stack = if is_proccessed_insn start_add
         result <- proccess_insn state bl_addr insn addr_stack
         case result of
           (new_state, Nothing, _) -> return new_state
-          (new_state, Just naddr, new_as) -> lift_block new_state bl_addr naddr new_as -- trace ("0x" ++ (showHex naddr "")) $
+          (new_state, Just naddr, new_as) -> lift_block new_state bl_addr naddr new_as (cnt + 1) cnt_max -- trace ("0x" ++ (showHex naddr "")) $
       otherwise -> trace ("---------- end block 0x" ++ showHex bl_addr "") $ return state
     return new_state
   where
@@ -112,14 +112,14 @@ check_grp_jump state (Insn insn) cur_block addr_stack = if mnemonic insn == "jmp
         then return (state, Skip insn, next_addr insn)
         else trace ((insn_to_str insn) ++ ";fork branch 0x" ++ (showHex jump_addr "")) $ do
           let right_addr = (next_addr insn)
-          lstate <- trace ("- lift subblock 0x" ++ showHex jump_addr " " ++ (show $ length addr_stack)) $ start_lift_block state jump_addr addr_stack
-          rstate <- trace ("- lift subblock 2 0x" ++ showHex right_addr "" ++ (show $ keys $ blocks lstate)) $ start_lift_block lstate right_addr addr_stack
+          lstate <- trace ("- lift subblock 0x" ++ showHex jump_addr " " ++ (show $ length addr_stack)) $ start_lift_block state jump_addr addr_stack 10
+          rstate <- trace ("- lift subblock 2 0x" ++ showHex right_addr "" ++ (show $ keys $ blocks lstate)) $ start_lift_block lstate right_addr addr_stack 10
           let (la, _):_ = List.filter skip_junk $ blocks rstate ! jump_addr
           let (ra, _):_ = List.filter skip_junk $ blocks rstate ! right_addr
           if la == ra
             then return (rstate, Skip insn, next_addr insn)
             else if (List.elem jump_addr $ keys $ blocks rstate) || (List.elem jump_addr $ blocks_queue rstate)
-              then return (rstate, Break insn, next_addr insn)
+              then return (rstate, Insn insn, next_addr insn) -- ??? fix infinity loop, right?
               else return (queue_block jump_addr rstate, Insn insn, next_addr insn)
       otherwise -> return (state, Break insn, next_addr insn)
 
