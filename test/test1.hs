1c63b4a7da748ff52ee2281eee3bfd6bd1901e3c
SD <ktion23@gmail.com>
2019-02-27 16:06:58 +0000

    .

--- test/test1.hs
+++ test/test1.hs
@@ -1,5 +1,20 @@
 module Test1 where
 
+import Test.Hspec
+import Test.QuickCheck
+import Control.Exception (evaluate)
+
+input = [0xB8, 0x01, 0x00, 0x00, 0x00, 0xB8, 0x02, 0x00, 0x00, 0x00]
+first_pass = FirstPass { buffer = input }
+simplification = Simplify { buffer = first_pass }
+
+main = hspec $ do
+discribe "Test1" $ do
+it "returns first_pass result" $ do
+first_pass shouldBe ([(setreg 'eax' (lit 1)), (setreg 'eax' (lit 2))])
+it "returns simplification result" $ do
+simplification shouldBe ("setreg 'eax' (lit 2)")
+
 {- |
 asm:
   mov eax, 1
@@ -15,12 +30,12 @@ after simplification:
 (setreg 'eax' (lit 2))
 -}
 
-input = { 0xB8, 0x01, 0x00, 0x00, 0x00, 0xB8, 0x02, 0x00, 0x00, 0x00 }
-
-first_pass = [
-  (setreg 'eax' (lit 1)),
-  (setreg 'eax' (lit 2))
-]
+-- input = { 0xB8, 0x01, 0x00, 0x00, 0x00, 0xB8, 0x02, 0x00, 0x00, 0x00 }
+--
+-- first_pass = [
+--   (setreg 'eax' (lit 1)),
+--   (setreg 'eax' (lit 2))
+-- ]
 
 -- output = (setreg 'eax' (lit 2))
 
@@ -39,9 +54,9 @@ after simplification:
   (setreg 'eax' (lit 2))
 -}
 
-input = { 0xB8, 0x01, 0x00, 0x00, 0x00, 0x83, 0xC0, 0x05, 0x83, 0xE8, 0x03 }
+-- input = { 0xB8, 0x01, 0x00, 0x00, 0x00, 0x83, 0xC0, 0x05, 0x83, 0xE8, 0x03 }
 
 -- output = (setreg 'eax' (lit 3))
 
-context = eval(output)
-register context eax == 3
+-- context = eval(output)
+-- register context eax == 3
