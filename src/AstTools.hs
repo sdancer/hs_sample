5c328743743215a98f4de076c8df66917a047cff
SD <ktion23@gmail.com>
2019-03-06 18:02:23 +0000

    filter flags

--- src/AstTools.hs
+++ src/AstTools.hs
@@ -2,8 +2,6 @@ module AstTools where
 
 import Ast
 
-
-
 filter_out_flags :: [[AstNodeType]] -> [[AstNodeType]]
 filter_out_flags asttree =
   --just used for some test, shouldn't be used in code as its not correct
