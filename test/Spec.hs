e6598b00829841d5eba62dad2a8cf805c277c968
SD <ktion23@gmail.com>
2019-03-06 17:11:33 +0000

    test

--- test/Spec.hs
+++ test/Spec.hs
@@ -5,6 +5,7 @@ import Test.Tasty.HUnit
 
 import Lifter
 import Ast
+import Hapstone.Internal.X86
 
 import Data.Word
 
@@ -16,21 +17,12 @@ safeHead (x:_) = Just x
 
 main :: IO ()
 main = defaultMain $
-  testCase "Example test case" $ do
-    -- assertion no. 1 (passes)
-    2 + 2 @?= 4
-    -- assertion no. 2 (fails)
+  testGroup "Lifter" [ test_simple_lift ]
+
+test_simple_lift =
+  testCase "simple lift buf" $ do
     l <- liftX86 [0xB8, 0x01, 0x00, 0x00, 0x00, 0xB8, 0x02, 0x00, 0x00, 0x00]
-    assertEqual "the list is not empty" [[(AssertNode "error")]] l
-    -- assertion no. 3 (would have failed, but won't be executed because
-    -- the previous assertion has already failed)
     l @?= [[SetReg (X86Reg X86RegEax) (BvNode 1 32)],[SetReg (X86Reg X86RegEax) (BvNode 2 32)]]
-
--- tests :: TestTree
--- tests = testGroup "Testing Examples" [unitTests]
-
--- unitTests :: TestTree
--- unitTests = testGroup "Tests (run via HUnit)" [ test_simple_lift ]
 
 liftX86 :: [Word8] -> IO [[AstNodeType]]
 liftX86 input = do
--- test/test1.hs
+++ test/test1.hs
@@ -1,23 +1,3 @@
-module Test1 where
-
-import Test.Hspec
-import Test.QuickCheck
-import Control.Exception (evaluate)
-
-import Lifter
-import Simplify
-
-input = [0xB8, 0x01, 0x00, 0x00, 0x00, 0xB8, 0x02, 0x00, 0x00, 0x00]
-first_pass = liftFromBuffer { buffer = input }
-simplification = Simplify { buffer = first_pass }
-
-main = hspec $ do
-discribe "Test1" $ do
-it "returns first_pass result" $ do
-first_pass shouldBe ([[SetReg (X86Reg X86RegEax) (BvNode 1 32)],[SetReg (X86Reg X86RegEax) (BvNode 2 32)]])
-it "returns simplification result" $ do
-simplification shouldBe (setreg 'eax' (lit 2))
-
 {- |
 asm:
   mov eax, 1
