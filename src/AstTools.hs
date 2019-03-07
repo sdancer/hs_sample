8f2263003ea2cb58ec684ca161ac2ed1c9403930
SD <ktion23@gmail.com>
2019-03-07 10:39:17 +0000

    .

--- src/AstTools.hs
+++ src/AstTools.hs
@@ -1,12 +1,13 @@
-module REAstTools where
+module AstTools where
 
 import Ast
 
 filter_out_flags :: [[AstNodeType]] -> [[AstNodeType]]
 filter_out_flags asttree =
   --just used for some test, shouldn't be used in code as its not correct
-  map asttree (\x ->
-    filter x (\y -> case y of
-      (SetFlag _ _) -> false
-      _ -> true
-    )
+  map (\x ->
+    filter (\y -> case y of
+      (SetFlag _ _) -> False
+      _ -> True
+      ) x
+    ) asttree
--- test/Spec.hs
+++ test/Spec.hs
@@ -9,7 +9,7 @@ import Hapstone.Internal.X86
 
 import Data.Word
 
-import RE.AstTools
+import AstTools
 
 
 safeHead :: [a] -> Maybe a
@@ -36,6 +36,8 @@ test_lift =
 test_simplify =
   testCase "asm: \n mov eax, 1 \n add eax, 5 \n sub eax, 3" $ do
         l1 <- liftX86toAst [0xB8, 0x01, 0x00, 0x00, 0x00, 0x83, 0xC0, 0x05, 0x83, 0xE8, 0x03]
-        (filter_out_flags l1) @?= [[SetReg (X86Reg X86RegEax) (BvNode 3 32)]]
-
-filter_out_flags
+        (filter_out_flags l1) @?= [
+          [SetReg (X86Reg X86RegEax) (BvNode 1 32)],
+          [SetReg (X86Reg X86RegEax) (BvaddNode (GetReg (X86Reg X86RegEax)) (BvNode 5 32))],
+          [AssertNode "sub"]
+          ]
