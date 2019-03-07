c757cbb108a391b4004b01ae7ebb1f170b4ad155
SD <ktion23@gmail.com>
2019-03-07 10:29:27 +0000

    .

--- src/AstTools.hs
+++ src/AstTools.hs
@@ -1,8 +1,12 @@
-module AstTools where
+module RE.AstTools where
 
 import Ast
 
 filter_out_flags :: [[AstNodeType]] -> [[AstNodeType]]
 filter_out_flags asttree =
   --just used for some test, shouldn't be used in code as its not correct
-  filter asttree (top most != SetFlag)
+  map asttree (\x ->
+    filter x (\y -> case y of
+      (SetFlag _ _) -> false
+      _ -> true
+    )
--- test/Spec.hs
+++ test/Spec.hs
@@ -9,7 +9,7 @@ import Hapstone.Internal.X86
 
 import Data.Word
 
-import Simplify
+import RE.AstTools
 
 
 safeHead :: [a] -> Maybe a
@@ -37,3 +37,5 @@ test_simplify =
   testCase "asm: \n mov eax, 1 \n add eax, 5 \n sub eax, 3" $ do
         l1 <- liftX86toAst [0xB8, 0x01, 0x00, 0x00, 0x00, 0x83, 0xC0, 0x05, 0x83, 0xE8, 0x03]
         (filter_out_flags l1) @?= [[SetReg (X86Reg X86RegEax) (BvNode 3 32)]]
+
+filter_out_flags
