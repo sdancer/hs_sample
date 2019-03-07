ef29d69ede74636ca84f159f11c6f80c3454907b
SD <ktion23@gmail.com>
2019-03-07 11:13:55 +0000

    .

--- test/Spec.hs
+++ test/Spec.hs
@@ -22,6 +22,7 @@ main = defaultMain $
   testGroup "Lifter" [
     test_lift
     , test_simplify
+    , test_eval
   ]
 
 
@@ -36,6 +37,16 @@ test_lift =
 test_simplify =
   testCase "asm: \n mov eax, 1 \n add eax, 5 \n sub eax, 3" $ do
         l1 <- liftX86toAst [0xB8, 0x01, 0x00, 0x00, 0x00, 0x83, 0xC0, 0x05, 0x83, 0xE8, 0x03]
+        (filter_out_flags l1) @?= [
+          [SetReg (X86Reg X86RegEax) (BvNode 1 32)],
+          [SetReg (X86Reg X86RegEax) (BvaddNode (GetReg (X86Reg X86RegEax)) (BvNode 5 32))],
+          [AssertNode "sub"]
+          ]
+
+test_eval =
+  testCase "eval" $ do
+        l1 <- liftX86toAst [0xB8, 0x01, 0x00, 0x00, 0x00, 0x83, 0xC0, 0x05, 0x83, 0xE8, 0x03]
+        --after running this code on a symbolic context, eax == 3
         (filter_out_flags l1) @?= [
           [SetReg (X86Reg X86RegEax) (BvNode 1 32)],
           [SetReg (X86Reg X86RegEax) (BvaddNode (GetReg (X86Reg X86RegEax)) (BvNode 5 32))],
