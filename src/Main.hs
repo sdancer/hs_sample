5039eb5dc31527b1180ecc14b2b2d5fd69c8d485
Murisi Tarusenga <murisit@gmail.com>
2019-03-19 15:42:37 +0200

    Put an example invocation of exec in Main.hs.

--- src/Main.hs
+++ src/Main.hs
@@ -13,15 +13,16 @@ main :: IO ()
 main = do
   -- contents <- BS.readFile "bs/blackcipher.aes"
   print "this should be in test/"
-  let input = [0xBC, 0x64, 0x00, 0x00, 0x00, 0x6A, 0x0A, 0x6A, 0x0D, 0x5B]
+  let input = [0xBC, 0x64, 0x00, 0x00, 0x00, -- mov esp,0x64
+              0x6A, 0x0A, -- push 0xa
+              0x6A, 0x0D, -- push 0xd
+              0x5B, -- pop ebx
+              0x59, -- pop ecx
+              0x01, 0xCB] -- add ebx,ecx 
   let modes = [Capstone.CsMode32]
   asm <- disasm_buf modes input
   case asm of
     Left _ -> print "error"
+    -- Register ebx will contain 23 as it is the result of 0xa+0xd
     Right b -> print (getRegisterValues (reg_file (run x86Context (concat (liftAsm modes b)))))
-  -- let simplification = Simplify { buffer = first_pass }
 
-  -- state <- lift_next_block $ new_state contents
-  --compile_blocks $ toAscList $ blocks state
-  -- print $ keys $ blocks state
-  --print $ blocks_queue state
