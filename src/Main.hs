40981a084d1a292f75da3a31e76b0b60b6efcafa
Murisi Tarusenga <murisit@gmail.com>
2019-03-26 09:17:51 +0200

    Documented machine code in Main.hs.

--- src/Main.hs
+++ src/Main.hs
@@ -11,9 +11,11 @@ import Lifter
 
 main :: IO ()
 main = do
-  -- contents <- BS.readFile "bs/blackcipher.aes"
   print "this should be in test/"
-  let input = [0xb8, 0x0a, 0x00, 0x00, 0x00, 0x83, 0xc0, 0x0a, 0xeb, 0xfb] -- jmp loop
+  let input = [0xb8, 0x0a, 0x00, 0x00, 0x00, -- mov eax,0xa
+              -- 00000005 <loop>:
+              0x83, 0xc0, 0x0a, -- add eax,0xa
+              0xeb, 0xfb] -- jmp loop
   let modes = [Capstone.CsMode32]
   asm <- disasm_buf modes input
   case asm of
