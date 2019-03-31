a867b5376cde678e051ed551fa8ffa3bc1e03b89
sd <ktion23@gmail.com>
2019-03-31 20:11:39 +0800

    naive dead code elimination

--- src/Phasses.hs
+++ src/Phasses.hs
@@ -2,15 +2,22 @@ module Phasses where
 
 import Data.List
 
-data SG = Set Int | Get Int deriving (Show, Eq)
+data SG = Set Int Int | Get Int deriving (Show, Eq)
 
 naive_dead_code_elimination =
   let folder ele (acc1, acc2) =
-        let cond_add = case (elem ele acc2) of
+        let cond_add a = case (elem a acc2) of
                       True -> (acc1, acc2) -- skipped
-                      False -> (acc1 ++ [ele], acc2 ++ [ele])
-            nacc2_delete = delete ele acc2
+                      False -> ([ele] ++ acc1, acc2 ++ [a])
+            nacc2_delete a = delete a acc2
         in  case ele of
-              Set a -> cond_add
-              Get a -> ([ele] ++ acc1, nacc2_delete)
-  in  foldr folder ([], []) [Set 1, Get 2, Get 3, Set 1]
+              Set a _ -> cond_add a
+              Get a -> ([ele] ++ acc1, nacc2_delete a)
+  in  foldr folder ([], []) [Set 5 1, Set 1 1, Set 1 2, Get 2, Get 1, Get 3, Set 1 3]
+
+{-
+fails on:
+set 1 1
+cond [ set 1 2 ]
+get 1
+-}
